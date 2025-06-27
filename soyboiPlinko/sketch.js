20192356e96e0e3fe5e7422acfd17f1bb3015bfe
Andrew Koerber <kerbybit@gmail.com>
2025-06-27 18:23:37 -0500

    Added option to click to make new soybois

--- soyboiPlinko/sketch.js
+++ soyboiPlinko/sketch.js
@@ -1,22 +1,25 @@
 var Engine = Matter.Engine,
   Runner = Matter.Runner,
   Bodies = Matter.Bodies,
-  Composite = Matter.Composite
+  Composite = Matter.Composite,
+  World = Matter.World
 
-var engine = Engine.create()
+let engine = Engine.create()
+let world = engine.world
 var objects = []
 var pegs = []
+var bois = []
 var window_width = 600
 var window_height = 600
 
-var soyboi = new Soyboi(200 + Math.random() * 100, 0, 18)
+bois.push(new Soyboi(200 + Math.random() * 100, 0, 18))
 for (x = 1; x < 11; x++) {
   for (y = 1; y < 11; y++) {
-    pegs.push(new Peg(x * 50 + (y % 2 * 25), y * 50))
+    pegs.push(new Peg(x * 50 + (y % 2 * 25), y * 50 + 50))
   }
 }
 
-Composite.add(engine.world, objects)
+Composite.add(world, [])
 
 var runner = Runner.create()
 Runner.run(runner, engine)
@@ -33,13 +36,24 @@ function draw() {
   pegs.forEach(peg => {
     peg.show()
   })
-  soyboi.show()
+  bois.forEach(boi => {
+    boi.show()
+  })
+  
+  push()
+  translate(mouseX-5, 0)
+  triangle(0, 0, 10, 0, 5, 10)
+  pop()
+}
+
+function mousePressed() {
+  bois.push(new Soyboi(mouseX, 0, 18))
 }
 
 function Peg(x, y) {
   this.r = 5
   this.body = Bodies.circle(x, y, this.r, {isStatic: true})
-  objects.push(this.body)
+  World.add(world, this.body)
 
   this.show = function() {
     var pos = this.body.position
@@ -54,7 +68,7 @@ function Peg(x, y) {
 function Soyboi(x, y, r) {
   this.r = r
   this.body = Bodies.circle(x, y, r, {mass: 10, restitution: 0.9})
-  objects.push(this.body)
+  World.add(world, this.body)
 
   this.show = function() {
     var pos = this.body.position
